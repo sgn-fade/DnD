33434b6aa8e8135c83057c3b6db7fb6de659a4e3
fade <dimabreusgo@gmail.com>
2024-11-24 02:09:30 +0200

    feat : game locations loop

--- scripts/Game.cs
+++ scripts/Game.cs
@@ -1,10 +1,27 @@
 using Godot;
 using System;
+using System.Linq;
+
 namespace DND;
 
 public partial class Game : Node
 {
-    public void StartGame(Scenario scenario){}
+    private Scenario _scenario;
+    public override void _Ready()
+    {
+        _scenario = LoadScenarioFromFile("fileExample.json");
+        StartGame(_scenario);
+    }
+
+    public void StartGame(Scenario scenario)
+    {
+        scenario.Locations.ForEach(LoopLocation);
+    }
     public void LoopLocation(Location location){}
+
+    public Scenario LoadScenarioFromFile(string filename)
+    {
+        return null;
+    }
 
 }
