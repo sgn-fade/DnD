b0ac0e0df3c51e479a58cdd2aff8ab5bbae43dfa
fade <dimabreusgo@gmail.com>
2024-11-24 02:11:29 +0200

    fix : weapon sprite agaist enum

--- scripts/Data/Weapon.cs
+++ scripts/Data/Weapon.cs
@@ -5,15 +5,8 @@ namespace DND;
 
 public class Weapon
 {
-    public enum WeaponType
-    {
-        Sword,
-        Daggers,
-        Staff,
-    }
-
     public String Name { get; set; }
     public String Description { get; set; }
-    public WeaponType Type { get; set; }
+    public Texture2D Sprite { get; set; }
     public Stat Stat { get; set; }
 }
