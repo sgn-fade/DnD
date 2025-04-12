1c5eeb198598a6482075068d6e727af5f0077075
fade <dimabreusgo@gmail.com>
2025-04-12 22:23:57 +0300

    Update SoundButton.cs

--- scripts/SoundButton.cs
+++ scripts/SoundButton.cs
@@ -3,5 +3,14 @@ using System;
 
 public partial class SoundButton : TextureButton
 {
+    [Export] private AudioStreamPlayer _player;
+    public override void _Ready()
+    {
+        Pressed += OnPressed;
+    }
 
+    private void OnPressed()
+    {
+        _player.Play();
+    }
 }
